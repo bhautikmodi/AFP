7551044379bf0e2a4687cdb3847b786cb234f4f2
Krupali Mistry <krupali.mistry@theopeneyes.in>
2018-04-17 11:17:48 +0530

    krupali

---
+++ js/custom.js
@@ -0,0 +1,53 @@
+<!-- INVITATION CODE -->
+$('.invitation_text .form-control').keyup(function(e){
+	if($(this).val().length==$(this).attr('maxlength'))
+	$(this).next('.form-control').focus()
+})
+<!-- END INVITATION CODE -->
+
+<!-- REGISTER EMPLOYEE -->
+$('#employee_btn').click(function () {
+	$("#submit_Modal").modal('show');
+});
+<!-- END REGISTER EMPLOYEE -->
+
+<!-- REGISTER COMPANY -->
+$('#company_btn').click(function () {
+	$("#submit_Modal").modal('show');
+});
+<!-- END REGISTER COMPANY -->
+
+<!-- SALES DASHBOARD -->
+$('.user_box').click(function(e){
+	$('.user_box').removeClass('active');
+    $(this).addClass('active');
+});
+<!-- END SALES DASHBOARD -->
+
+<!-- PROGRESS BAR ASSESSMENT -->
+progress = document.getElementById("progress");
+$(progress).css("width", "30%");
+<!-- END PROGRESS BAR ASSESSMENT -->
+
+<!-- ASSESSMENT -->
+$('input[type="radio"]').change(function(){
+	var i;
+	for (i = 0; i < 51; i++) { 
+		if($("input:radio[name='ksa" + i + "_radio']").is(":checked")) {
+		  $('#ksa' + i + '_dots').removeClass('fa-circle-o');
+		  $('#ksa' + i + '_dots').addClass('fa-dot-circle-o');
+	  }
+	}
+});
+<!-- END ASSESSMENT -->
+
+<!-- TOOLTIP -->
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
+<!-- END TOOLTIP -->
+
+<!-- SCROLLBAR -->
+ new PerfectScrollbar('.preview_ksa tbody');
+ new PerfectScrollbar('.course_rec tbody');
+<!-- END SCROLLBAR -->

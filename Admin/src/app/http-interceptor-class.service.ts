693074b4d4c34812d232c20bb86aae7df0326f32
OPENEYES\Developer7 <vidhi.bathani@theopeneyes.in>
2018-04-19 10:57:31 +0530

    domain

--- Admin/src/app/http-interceptor-class.service.ts
+++ Admin/src/app/http-interceptor-class.service.ts
@@ -1,19 +1,38 @@
 import {Injectable} from '@angular/core';
-import {HttpEvent, HttpInterceptor, HttpHandler, HttpRequest} from '@angular/common/http';
-import {Observable} from "rxjs";
+import {HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpResponse, HttpErrorResponse} from '@angular/common/http';
+import {Observable} from "rxjs/Observable";
+import { Router } from '@angular/router';
+import 'rxjs/add/operator/do';
 
 @Injectable()
 export class HttpInterceptorClassService implements HttpInterceptor { 
-  constructor(){
+  constructor(private router: Router){
 
   }
-  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> { debugger
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> { 
     req = req.clone({
       setHeaders: {
-        Authorization: `Bearer ${localStorage.getItem('token')}`
+        Authorization: `${localStorage.getItem('token')}`
       }
     });
 
-    return next.handle(req);
+    return next.handle(req).do((event: HttpEvent<any>) => { debugger
+      if (event instanceof HttpResponse) {
+        if(event.body.error_code==2008){
+          return this.router.navigate(['/dashboard']);
+        } else {
+          return next.handle(req);
+        }        
+      }
+    }, (err: any) => {
+      if (err instanceof HttpErrorResponse) {
+        if (err.status === 406) {
+          // redirect to the login route
+          // or show a modal
+        }
+        return next.handle(req);
+      }
+    });
+    
   }
 } 

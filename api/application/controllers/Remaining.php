8efb4ea47c11411d9c18a3be8b4fd5e353ee095f
OPENEYES\Developer5 <mitesh.patel@theopeneyes.in>
2018-04-20 16:08:17 +0530

    mm

--- api/application/controllers/Remaining.php
+++ api/application/controllers/Remaining.php
@@ -1,8 +1,5 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-header("Access-Control-Allow-Origin: *");
-header('Access-Control-Allow-Headers: *');
-header('Content-Type: application/json');
 
 class Remaining extends CI_Controller 
 {	

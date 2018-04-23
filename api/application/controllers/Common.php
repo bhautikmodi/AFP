17a8cd139257fd5b4c92d5351f9a3ca3a94c7bf6
OPENEYES\Developer2 <nirav.patell@theopeneyes.in>
2018-04-23 11:53:10 +0530

    x

--- api/application/controllers/Common.php
+++ api/application/controllers/Common.php
@@ -12,6 +12,32 @@ class Common extends CI_Controller {
 		$this->load->model('Common_model');
 		
 	}
+
+	public function hello() {
+
+		$data = $this->db->query('SELECT DomainId FROM tbldomainwiseksa group by DomainId');
+		
+		//echo "<pre>"; print_r($data->result()); 
+
+				
+				//echo "<pre>"; print_r($data1->result()); 
+
+				foreach($data->result() as $row)
+		 {
+			$data1 = $this->db->query('SELECT ConfigurationId,`Key`,`Value` FROM tblmstconfiguration AS cl WHERE cl.Key = "CourseLevel"');
+			foreach($data1->result() as $row1)
+		 {
+			 
+			
+			$data3 = $this->db->query('SELECT c.CourseId,c.Name,c.CourseLevelId FROM tblmstcourse AS c WHERE c.DomainId = '.$row->DomainId.' && c.CourseLevelId = '.$row1->ConfigurationId);
+				echo "<pre>"; print_r($data3->result()); 
+		 }
+			
+			echo "<br/>"; 
+		 }
+
+
+	} 
 	
 	public function get_permissiondata() {
 		

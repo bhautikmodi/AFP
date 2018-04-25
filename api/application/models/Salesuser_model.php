9e87d89c3d4760ea14ab4f2d568ff238d8c35fce
OPENEYES\Developer2 <nirav.patell@theopeneyes.in>
2018-04-25 16:37:47 +0530

    ss

--- api/application/models/Salesuser_model.php
+++ api/application/models/Salesuser_model.php
@@ -313,6 +313,31 @@ class Salesuser_model extends CI_Model
             return false;
         }
     }
-
+    public function getallcourse($CAssessmentId = NULL) {
+		
+		if($CAssessmentId) {
+            $data = $this->db->query('SELECT DomainId,Name FROM tblmstdomain');
+            $obj = '';		
+            foreach($data->result() as $row)
+            {	
+                $domain = array();
+                $array = json_decode(json_encode($row), True);
+                $domain = $array;
+                //$data1 = $this->db->query('SELECT ConfigurationId,`Key`,`Value` FROM tblmstconfiguration AS cl WHERE cl.Key = "CourseLevel" AND ((cl.Value = "Foundational" AND '.$row->avg.' in (0,1,2)) OR  (cl.Value = "Intermediate" AND '.$row->avg.' in (0,1,2,3)) OR (cl.Value = "Advanced" AND '.$row->avg.' in (0,1,2,3,4)))');
+                $data1 = $this->db->query('SELECT rs.Name as rnm, (SELECT c.ConfigurationId FROM tblmstconfiguration AS c WHERE c.Key="CourseLevel" AND (CASE WHEN (rs.Name="Developing" OR rs.Name="General Awareness") THEN c.value="Foundational" WHEN rs.Name="Intermediate" THEN c.value="Intermediate" WHEN rs.Name="Advanced" THEN c.value="Advanced" END)) as ConfigurationId FROM tblmstratingscale AS rs');
+                $domain['rs'] = array();                
+                foreach($data1->result() as $row1)
+                {	                    
+                    $array1 = json_decode(json_encode($row1), True);
+                    $data3 = $this->db->query('SELECT c.CourseId,c.Name as cnm,c.CourseLevelId FROM tblmstcourse AS c WHERE c.DomainId = '.$row->DomainId.' && c.CourseLevelId = '.$row1->ConfigurationId);
+                    $array2 = json_decode(json_encode($data3->result()), True);
+                    $array1['course'] = $array2;
+                    $domain['rs'][] = $array1;
+                }
+                $obj[] = $domain; 
+            }  
+            return $obj;
+        }				
+    }
  }
  

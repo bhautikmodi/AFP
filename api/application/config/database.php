cff45a0f07c9b0b0fc5e8d10e9e65373f614e7d0
OPENEYES\Developer5 <mitesh.patel@theopeneyes.in>
2018-04-25 17:47:41 +0530

    k

--- api/application/config/database.php
+++ api/application/config/database.php
@@ -71,7 +71,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | the query builder class.
 */
 
-$active_group = 'Default';
+$active_group = 'default';
+
 
 $query_builder = TRUE;
 

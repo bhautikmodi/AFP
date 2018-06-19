0da49c4c77bc154ae07f5e215a6f1718c68cf0e6
vidhi1293 <35034821+vidhi1293@users.noreply.github.com>
2018-06-19 13:11:41 +0530

    uhb

--- Admin/src/app/domain-list/domain-list.component.ts
+++ Admin/src/app/domain-list/domain-list.component.ts
@@ -6,7 +6,7 @@ import { DomainService } from '../services/domain.service';
 import { CommonService } from '../services/common.service';
 import { Globals } from '.././globals';
 
-declare var $,unescape: any;
+declare var $,unescape	: any;
 
 @Component({
   selector: 'app-domain-list',
